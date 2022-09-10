d831b1d2823b1d5dd2d929fda5e7a3cb73c68589
FrankTCA <frank@steamedveggie.com>
2022-09-10 11:03:19 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <script src="js/jquery-3.6.1.min.js"></script>
+    <style>
+        .bg {
+            background-image: url("https://imgdrop.infotoast.org/crimages/<?php
+            echo rand(0, 606);
+?>.jpg");
+        }
+        .time {
+            position: absolute;
+            bottom: 0;
+            right: 0;
+        }
+        #theTime {
+            font-family: Arial, sans-serif;
+            font-size: xxx-large;
+        }
+    </style>
+    <script>
+        $(document).ready(function() {
+            setInterval(function() {
+                let currentDate = new Date();
+                let time = currentDate.getHours() + ":" + currentDate.getMinutes();
+                $('#theTime').innerText = time;
+            })
+        })
+    </script>
+</head>
+<body class="bg">
+<div class="time">
+    <span id="theTime"></span>
+</div>
+</body>
+</html>
